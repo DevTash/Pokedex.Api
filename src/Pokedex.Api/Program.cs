743042000c1f137572b8eb82eee429ed659e6fcd
Tashan <tashan.lewis@gandr.me>
2021-07-12 21:52:14 +0100

    Re-org & test

--- src/Pokedex.Api/Program.cs
+++ src/Pokedex.Api/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Pokedex
+namespace Pokedex.Api
 {
     public class Program
     {
--- src/Pokedex.Api/Startup.cs
+++ src/Pokedex.Api/Startup.cs
@@ -12,7 +12,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
-namespace Pokedex
+namespace Pokedex.Api
 {
     public class Startup
     {
